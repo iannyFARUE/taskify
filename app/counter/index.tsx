c1e72ecfa73a7cdd1b9caa43d8cf950aa1930643
Ian Farai Madhara <iannyfarai@gmail.com>
2026-07-26 13:57:53 -0400

    added option to add shopping item

---
+++ app/counter/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function CounterScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Counter</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: { fontSize: 18, fontWeight: "200" },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,47 @@
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { theme } from "../theme";
-import { StyleSheet, View } from "react-native";
-import { Link } from "expo-router";
+import { StyleSheet, TextInput, View } from "react-native";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState(initialList);
+  const [value, setValue] = useState<string>();
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue(undefined);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Link
-        href="/counter"
-        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
-      >
-        Go to /counter
-      </Link>
-      <ShoppingListItem name="Coffee" isCompleted />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Milk" isCompleted />
+      <TextInput
+        value={value}
+        style={styles.textInput}
+        onChangeText={setValue}
+        placeholder="E.g Coffee"
+        onSubmitEditing={handleSubmit}
+        returnKeyType="done"
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem name={item.name} key={item.id} />
+      ))}
     </View>
   );
 }
@@ -23,6 +50,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    marginHorizontal: 12,
+    marginBottom: 12,
   },
 });
