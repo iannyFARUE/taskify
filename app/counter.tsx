3415511b9043322f628c0fc0f11bf5c29f2a75bd
Ian Farai Madhara <iannyfarai@gmail.com>
2026-07-26 00:16:28 -0400

    added routing modal

---
+++ app/counter.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "expo-router";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+
+export default function CounterScreen() {
+  const router = useRouter();
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.navigate("/idea")}>
+        <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
+          Go to /idea
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
-import { ShoppingListItem } from "./components/ShoppingListItem";
-import { theme } from "./theme";
+import { ShoppingListItem } from "../components/ShoppingListItem";
+import { theme } from "../theme";
 import { StyleSheet, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
+      >
+        Go to /counter
+      </Link>
       <ShoppingListItem name="Coffee" isCompleted />
       <ShoppingListItem name="Tea" />
       <ShoppingListItem name="Milk" isCompleted />
